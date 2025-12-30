7ab8c46b7650dcb0ca7b815744fd01d2d83a6cce
abdullah2776 <aboodqubbaj2005@gmail.com>
2025-12-30 16:17:17 +0300

    finishing lab

---
+++ StudentManagementSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StudentManagementSystem.Models;
+
+namespace StudentManagementSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<StudentManagementSystem.Models.Student> Student { get; set; } = default!;
+    }
+}
---
+++ StudentManagementSystem/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagementSystem.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Student Name")]
+        public string FullName { get; set; }
+
+        [Required]
+        [Display(Name = "Student ID")]
+        public string StudentNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Program { get; set; }
+    }
+}
